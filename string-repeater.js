e881e8dcab9982f65455b4140e655aa61c87bb24
stephen1521 <stephen.warnock@codeimmersives.com>
2022-09-21 15:10:32 -0400

    updating file

--- string-repeater.js
+++ string-repeater.js
@@ -2,6 +2,6 @@ const prompt = require('prompt-sync')({sigint: true});
 let input = prompt('Enter a string: ');
 let str = input
 while(input.length <= 10) {
-    input += str;
     console.log(input);
+    input += str;
 }
